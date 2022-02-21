735158ed47292df465adaac53338ca498299d94b
Vespertino <Vespertino@C05PC01.informatica.comercio>
2022-02-21 17:17:46 +0100

    Commit getMarca

---
+++ Practica_Efrain/src/EfrainCumbicusCoches.java
@@ -0,0 +1,19 @@
+
+public class EfrainCumbicusCoches {
+	public String marca;
+	public	String modelo;
+	public int añoFab;
+	
+	public EfrainCumbicusCoches(String marca, String modelo, int añoFab){
+		this.marca=marca;
+		this.modelo=modelo;
+		this.añoFab=añoFab;
+		
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	
+}
